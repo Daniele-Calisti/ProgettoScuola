2bddc97312049c3303e9a1d9b0719a05fbd00691
Daniele-Calisti <58037698+Daniele-Calisti@users.noreply.github.com>
2020-04-21 20:12:33 +0200

    Update confermaEmail.php

--- public/php/confermaEmail.php
+++ public/php/confermaEmail.php
@@ -6,13 +6,20 @@
 	//stabilisco la connesione al database
 	include 'dbConn.php';
 
-		$userFunc = new userClass($pdo,'credenziali');	//Instanza della classe per verificare l'account dell'utente
+		$userFunc = new userClass($pdo,'credenziali','','',$_GET['email']);	//Instanza della classe per verificare l'account dell'utente, lascio 2 spazi vuoti che sarebbero username e password che non servono
 		//Passo come parametri l'email e il token preso dal'url
 		$esito = $userFunc->verificaAccount($_GET['email'],$_GET['token']);
 		
 		if($esito)
-			header('location : /homepage/accountVerificato');		//Rimando ad una ipotetica homepage con messaggio di avvenuta verifica dell'account
+        {
+        	$user = $userFunc->getUser();	
+        	$_SESSION['user'] = $user;
+        	header('location : /homepage/accountVerificato');		//Rimando ad una ipotetica homepage con messaggio di avvenuta verifica dell'account
+        }
 		else
-			header('location : /homepage/erroreAccount');			//Rimando ad un homepage con errore di verifica account
+        {
+        	header('location : /homepage/erroreAccount');			//Rimando ad un homepage con errore di verifica account
+        }
+			
 
 ?>
