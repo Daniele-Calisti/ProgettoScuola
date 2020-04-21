f48c1d311fced17b9fa6ca2de18f43067ab3ab30
Daniele-Calisti <58037698+Daniele-Calisti@users.noreply.github.com>
2020-04-21 20:10:40 +0200

    Update createTable.sql

--- public/sql/createTable.sql
+++ public/sql/createTable.sql
@@ -1,1 +1,8 @@
-CREATE TABLE `progettoweb`.`credenziali` ( `id` INT NULL AUTO_INCREMENT , `utente` VARCHAR(255) NULL ,`password` VARCHAR(255) NULL, `email` VARCHAR(255) NULL , `verificato` TINYINT(1) NULL COMMENT 'assume valore 1 o 0' , `token` VARCHAR(255) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `progettoweb`.`credenziali` ( `id` INT NULL AUTO_INCREMENT ,
+                                            `utente` VARCHAR(255) NULL ,
+                                            `password` VARCHAR(255) NULL, 
+                                            `email` VARCHAR(255) NULL , 
+                                            `verificato` TINYINT(1) NULL COMMENT 'assume valore 1 o 0' , 
+                                            `token` VARCHAR(255) NULL , 
+                                            PRIMARY KEY (`id`)) ENGINE = InnoDB;
+                                          )
